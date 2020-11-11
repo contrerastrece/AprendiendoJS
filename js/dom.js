eaa523e1fb014eabcdd35af8d1ded2a1ddb8fc9b
joel <joel.vicente.quispe@gmail.com>
2020-11-11 13:51:38 -0500

    Nodos, Selectores, Elementos

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,23 @@
 // elementos del document
-console.log(document)
-console.log(document.head)
-console.log(document.body)
-console.log(document.documentElement)
-console.log(document.title)
+
+// console.log(document)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.documentElement)
+// console.log(document.title)
+
+
+
+
+
+
+// NODOS ELEMENTOS SELECTORES
+
+console.log(document.getElementsByTagName("li"))//devolverá una coleccion de elementos de LI
+console.log(document.getElementsByClassName("card"))
+console.log(document.getElementsByName("menu"))
+console.log(document.getElementById("menu"))
+console.log(document.querySelector(".card"))//devuelve la primera que tiene la clase card
+console.log(document.querySelectorAll(".card"))
+console.log(document.querySelectorAll("#menu"))
+document.querySelectorAll(".card").forEach(el=>console.log(el));//devolverá el codigo HTML que contengan las clases card
