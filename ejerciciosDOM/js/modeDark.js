ced1a1c61378e624f47bd0f0eccf0fa15793038e
vcontreras <contrerastrece@gmail.com>
2020-12-10 01:32:37 -0500

    modo Dark con localStorage

--- ejerciciosDOM/js/modeDark.js
+++ ejerciciosDOM/js/modeDark.js
@@ -1,17 +1,33 @@
 export {modeDark}
 function modeDark(e,btn){
     const $btn=document.querySelector(btn);
-    document.addEventListener("click",e=>{                   
-        if(e.target.matches(btn)){
-            document.querySelector("body").classList.toggle("dark")           
-            if($btn.classList.contains("bxs-toggle-right")){
-                $btn.classList.remove("bxs-toggle-right")
-                $btn.classList.toggle("bx-toggle-left")
-            }else{
-                $btn.classList.add("bxs-toggle-right")                
-                $btn.classList.remove("bx-toggle-left")
-            }        
-        }
-    })
     
+    //capturar el theme actual 
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme === 'dark') {
+        document.body.classList.toggle('dark');
+        $btn.classList.replace("bx-toggle-right","bx-toggle-left")
+    }
+   
+    document.addEventListener("click",e=>{                      
+        if(e.target.matches(btn)){        
+            let theme;  
+
+            //por cada click intercambiara de theme
+            document.body.classList.toggle("dark")
+
+            // intercambio de clases para el toggle
+            $btn.classList.contains("bx-toggle-right")
+                ?$btn.classList.replace("bx-toggle-right","bx-toggle-left")   
+                :$btn.classList.replace("bx-toggle-left","bx-toggle-right");            
+            
+            //si body ontiene la clase "dark" le asignamos a la variable theme el valor de dark en el storage
+            document.body.classList.contains("dark")
+                ?theme = "dark"
+                :theme= "light";      
+            
+            //asignamos nuestro nuevo valor de theme
+            localStorage.setItem("theme", theme);  
+        }
+    })    
 }
