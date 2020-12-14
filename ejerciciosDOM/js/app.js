42d72110a189ac097d6b20b1a80c0b46e1149c9f
vcontreras <contrerastrece@gmail.com>
2020-12-14 16:24:44 -0500

    responsive Tester v2

--- ejerciciosDOM/js/app.js
+++ ejerciciosDOM/js/app.js
@@ -15,7 +15,8 @@ document.addEventListener("DOMContentLoaded",e=>{
     modeDark(e,"#btnMode");
     responsiveMedia("video","(min-width:600px)",`<a target="_blank" href="https://www.youtube.com/watch?v=n7ZuRd-Mjg8">ver video</a>`,`<iframe width="560" height="315" src="https://www.youtube.com/embed/n7ZuRd-Mjg8" frameborder="0" allow="accelerometer"; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`);    
     responsiveMedia("mapa","(min-width:1025px)",`<a target="_blank" href="https://goo.gl/maps/afz6oR7W7ptoeQDo8">ver Mapa</a>`,`<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3036.9589583960087!2d116.5681862149428!3d40.4319076793633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35f121d7687f2ccf%3A0xd040259b950522df!2sMuralla%20China!5e0!3m2!1ses-419!2spe!4v1607811431153!5m2!1ses-419!2spe" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>`);
-    responsiveTester("url","ancho","alto","#probar","#cerrar");
+    // responsiveTester("url","ancho","alto","#probar","#cerrar");
+    responsiveTester("form");
 });
 
 document.addEventListener("keydown",e=>{
--- ejerciciosDOM/js/responsiveTester.js
+++ ejerciciosDOM/js/responsiveTester.js
@@ -1,36 +1,58 @@
 export {responsiveTester}
 
-function responsiveTester(url,ancho,alto,btnProbar,btnCerrar){
-    let ventanaUrl;
-    let $url=document.getElementById(url);
-    let $ancho=document.getElementById(ancho);
-    let $alto=document.getElementById(alto);
-        //hacemos que cargue con el contenido vacio cada input
-        $url.value="";
-        $alto.value="";
-        $ancho.value="";
+// function responsiveTester(url,ancho,alto,btnProbar,btnCerrar){
+//     let ventanaUrl;
+//     let $url=document.getElementById(url);
+//     let $ancho=document.getElementById(ancho);
+//     let $alto=document.getElementById(alto);
+//         //hacemos que cargue con el contenido vacio cada input
+//         $url.value="";
+//         $alto.value="";
+//         $ancho.value="";
         
-        document.querySelector(btnCerrar).disabled=true;  
-    document.addEventListener("click",e=>{        
+//         document.querySelector(btnCerrar).disabled=true;  
+//     document.addEventListener("click",e=>{        
 
-        //daremos un ancho y alto a nuestra ventana
-        let configuracion_ventana = `width=${$ancho.value},height=${$alto.value}`;
-        if(e.target.matches(btnProbar)){
-            ventanaUrl=open($url.value,"ContrerasPariona",configuracion_ventana);
-            //deshabilitar el btnProbar, y habilitar el btnCerrar
-            document.querySelector(btnProbar).disabled=true;  
-            document.querySelector(btnCerrar).disabled=false;      
-        }
-        if(e.target.matches(btnCerrar)){
-            ventanaUrl.close();    
+//         //daremos un ancho y alto a nuestra ventana
+//         let configuracion_ventana = `width=${$ancho.value},height=${$alto.value}`;
+//         if(e.target.matches(btnProbar)){
+//             ventanaUrl=open($url.value,"ContrerasPariona",configuracion_ventana);
+//             //deshabilitar el btnProbar, y habilitar el btnCerrar
+//             document.querySelector(btnProbar).disabled=true;  
+//             document.querySelector(btnCerrar).disabled=false;      
+//         }
+//         if(e.target.matches(btnCerrar)){
+//             ventanaUrl.close();    
 
-            $url.value="";
-            $ancho.value="";
-            $alto.value="";
-            //Habilitamos el btnProbar
-           document.querySelector(btnProbar).disabled=false;
-           document.querySelector(btnCerrar).disabled=true;    
+//             $url.value="";
+//             $ancho.value="";
+//             $alto.value="";
+//             //Habilitamos el btnProbar
+//            document.querySelector(btnProbar).disabled=false;
+//            document.querySelector(btnCerrar).disabled=true;    
+//         }
+//     })
+    
+// }
+
+function responsiveTester(form){
+    const $form=document.getElementById(form);
+    let $url;
+    document.addEventListener("submit",e=>{
+        //comparar el objeto que orgina el evento contra
+        //la variable que guarda la referencia (elemento del DOM)
+        if(e.target===$form){
+            //cancelar el envio de los datos del formulario por metodo get
+            e.preventDefault();
+            let configuracion_ventana=`innerWidth=${$form.ancho.value}, innerHeight=${$form.alto.value}`;
+            $url=open($form.url.value,"Ventana",configuracion_ventana);
+            // alert($form.url.value)
         }
     })
-    // console.log($alto.value);
+    document.addEventListener("click",e=>{
+        if(e.target===$form.cerrar){ $url.close()}
+        // alert($form.cerrar)}
+        // console.log($form.cerrar);
+    })
+
 }
